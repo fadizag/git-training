2735ff2b4473a7d3e7751a0ae7d77d49d2c55cc9
fadizag <fadizagharneh@gmail.com>
2026-08-15 17:06:57 +0300

    Merge remote changesfffffff

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fetch Test From GitHub',
+      title: 'Products Feature',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
