b7321aa0884393d550437546c947cf09fdd879e2
fadizag <fadizagharneh@gmail.com>
2026-08-15 16:05:01 +0300

    Add products feature

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter after first push',
+      title: 'Products Feature',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
