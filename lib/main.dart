253d59751879f18c8f2c681517b9b39525bf586b
fadizag <fadizagharneh@gmail.com>
2026-08-15 16:18:07 +0300

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Products Feature',
+      title: 'Conflict from feature',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
